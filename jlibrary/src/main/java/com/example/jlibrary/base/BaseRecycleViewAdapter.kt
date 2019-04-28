666aa8891d83eac028a4788ca5e51e323f98d72f
xiaodong.jin <1486336994@qq.com>
2019-04-28 19:37:18 +0800

    update to androidx

--- jlibrary/src/main/java/com/example/jlibrary/base/BaseRecycleViewAdapter.kt
+++ jlibrary/src/main/java/com/example/jlibrary/base/BaseRecycleViewAdapter.kt
@@ -1,6 +1,14 @@
 package com.example.jlibrary.base
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
 /**
  * Created by xiaodong.jin on 2019/4/16.
  * description：
  */
+class BaseRecycleViewAdapter{
+
+
+
+}
--- jlibrary/src/main/java/com/example/jlibrary/base/BaseViewHold.kt
+++ jlibrary/src/main/java/com/example/jlibrary/base/BaseViewHold.kt
@@ -4,3 +4,4 @@ package com.example.jlibrary.base
  * Created by xiaodong.jin on 2019/4/16.
  * description：
  */
+class BaseViewHold
